c94b73f91b0ced9959dd6cc11c995a1c53bdef8d
oseimacdonald <oseimacdonald@gmail.com>
2026-01-24 19:15:06 +0000

    newcheck

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-import styles from '@/app/ui/home.module.css';
+import styles from '@/ui/home.module.css';
 import Status from '@/ui/status';
 import { lusitana } from '@/ui/fonts';
 import Image from 'next/image';
